935be3ca9cfcb5b482e1b46205a7fd5edf7fb481
Cristina Rottondi <crottondi@legionlogin.local>
2021-06-03 01:23:00 +0200

    scriptino_comodo

--- SBATCH_SCRIPTS/4_TO_PKLS/1_build_distances_many_chunks.sh
+++ SBATCH_SCRIPTS/4_TO_PKLS/1_build_distances_many_chunks.sh
@@ -1,62 +1,7 @@
-sed -e 's/{NUMBER}/0/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/1/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/2/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/3/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/4/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/5/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/6/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/7/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/8/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/9/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/10/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/11/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/12/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/13/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/14/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/15/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/16/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/17/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/18/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/19/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/20/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/21/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/22/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/23/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/24/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/25/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/26/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/27/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/28/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
-sed -e 's/{NUMBER}/29/g' 1_build_distances_chunk.sbatch > tmp.sbatch
-sbatch tmp.sbatch
+for i in {0..3}
+do 
+    sed -e 's/{NUMBER}/'$i'/g' 1_build_distances_chunk.sbatch > tmp.sbatch
+    sbatch tmp.sbatch
+done
 
 rm tmp.sbatch

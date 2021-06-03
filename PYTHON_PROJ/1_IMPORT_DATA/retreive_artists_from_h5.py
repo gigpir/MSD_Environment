23e1771fbf2fba855ef6d92199885b88b1c0c965
gigi <lpgpirisi@gmail.com>
2021-06-03 02:29:21 +0200

    min fix

--- PYTHON_PROJ/1_IMPORT_DATA/retreive_artists_from_h5.py
+++ PYTHON_PROJ/1_IMPORT_DATA/retreive_artists_from_h5.py
@@ -10,10 +10,7 @@ import argparse
 def main(args):
     input_folder = args.i_path
 
-    if args.o_path[-1] == '/':
-        output_filename = args.o_path + args.o_name
-    else:
-        output_filename = os.path.join(args.o_path , args.o_name)
+    output_filename = os.path.join(args.o_path, args.o_name)
 
     artists = retrieve_artist_dict(basedir=input_folder)
 
@@ -25,7 +22,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--i_path', required=True, type=str, help='path to data directory of MSD')
+    parser.add_argument('--i_path', required=True, type=str, help='path to data directory /data of MSD')
     parser.add_argument('--o_path', required=True, type=str, help='path where MSD pkl will be saved')
     parser.add_argument('--o_name', required=True, type=str, help='output filename')
     args = parser.parse_args()

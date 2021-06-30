4e41fb7256ad0dc9980af7245b166476a8bcd98c
Cristina Rottondi <crottondi@legionlogin.local>
2021-06-30 10:43:24 +0200

    sbatch update

--- SBATCH_SCRIPTS/5_EVAL/evaluate_terms_intersection.sh
+++ SBATCH_SCRIPTS/5_EVAL/evaluate_terms_intersection.sh
@@ -1,9 +1,27 @@
-vect=(100 200 400 800 1600 3200 6400 12800)
+mto=(100 200 400 800 1600)
+md=( "NORM_STD" "NORM_OTHER" "NOT_NORM" )
+vls=( "-1" "500")
 
-for i in ${vect[*]} 
-do 
-    sed -e 's/{VAR}/'$i'/g' evaluate_terms_intersection.sbatch > tmp.sbatch
-    sbatch tmp.sbatch
+for i in ${mto[*]} 
+do  
+    for j in ${md[*]} 
+    do    
+        for k in ${vls[*]} 
+        do 
+        rm tmp.sbatch tmp1.sbatch tmp2.sbatch 
+        sed -e 's/{MTO}/'$i'/g' evaluate_terms_intersection_cc_peak_1.sbatch > tmp.sbatch
+        
+        sed -e 's/{MD}/'$j'/g' tmp.sbatch > tmp1.sbatch
+        
+        sed -e 's/{VLS}/'$k'/g' tmp1.sbatch > tmp2.sbatch
+        
+        sbatch tmp2.sbatch
+
+        done
+    
+    done
+    
+    
 done
 
-rm tmp.sbatch
+rm tmp.sbatch tmp1.sbatch tmp2.sbatch 
